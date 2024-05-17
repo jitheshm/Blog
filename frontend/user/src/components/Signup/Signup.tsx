7d018dc4dacc6b16ddb788f8d1fe9d3a7ff35784
jitheshm <jitheshmjithooz@gmail.com>
2024-05-17 20:34:19 +0530

    Signup form updated

--- frontend/user/src/components/Signup/Signup.tsx
+++ frontend/user/src/components/Signup/Signup.tsx
@@ -1,8 +1,18 @@
+"use client"
+
+import Link from "next/link"
+import { useState } from "react"
 
 
 function Signup() {
 
+    const [fullName, setFullName] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
 
+    const handleSubmit = () => {
+
+    }
     return (
         <div className="" style={{ backgroundColor: "rgb(229 229 229 / 41%)" }}>
             <section className="vh-100 ">
@@ -18,25 +28,25 @@ function Signup() {
                                         <p className="text-white-50 mb-3">Please enter your Details</p>
                                         <div data-mdb-input-init className="form-outline form-white mb-2">
                                             <label className="form-label" htmlFor="">Full Name</label>
-                                            <input type="text" id="" className="form-control form-control-lg" />
+                                            <input type="text" id="" className="form-control form-control-lg" value={fullName} onChange={(e) => { setFullName(e.target.value) }} />
 
                                         </div>
                                         <div data-mdb-input-init className="form-outline form-white mb-2">
                                             <label className="form-label" htmlFor="typeEmailX">Email</label>
-                                            <input type="email" id="typeEmailX" className="form-control form-control-lg" />
+                                            <input type="email" id="typeEmailX" className="form-control form-control-lg" value={email} onChange={(e) => { setEmail(e.target.value) }} />
 
                                         </div>
                                         <div data-mdb-input-init className="form-outline form-white mb-2">
                                             <label className="form-label" htmlFor="typePasswordX">Password</label>
-                                            <input type="password" id="typePasswordX" className="form-control form-control-lg" />
+                                            <input type="password" id="typePasswordX" className="form-control form-control-lg" value={password} onChange={(e) => { setPassword(e.target.value) }} />
 
                                         </div>
-                                       
-                                        <button data-mdb-button-init data-mdb-ripple-init className="btn btn-dark btn-lg px-5" type="submit">Create</button>
+
+                                        <button data-mdb-button-init data-mdb-ripple-init className="btn btn-dark btn-lg px-5" type="button" onClick={handleSubmit}>Create</button>
 
                                     </div>
                                     <div>
-                                        <p className="mb-0">Have an account? <a href="#!" className="text-dark-50 fw-bold">Sign In</a>
+                                        <p className="mb-0">Have an account? <Link href="/signin" className="text-dark-50 fw-bold">Sign In</Link>
                                         </p>
                                     </div>
                                 </div>
