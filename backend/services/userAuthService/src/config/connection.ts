c575de2dbe918e77ade378320fb68921cac4ce58
jitheshm <jitheshmjithooz@gmail.com>
2024-05-18 21:57:30 +0530

    Add Dockerfile, .dockerignore, and compose.yaml files, nodemon installed and configured,database connection updated

--- backend/services/userAuthService/src/config/connection.ts
+++ backend/services/userAuthService/src/config/connection.ts
@@ -4,12 +4,16 @@ import mongoose from "mongoose";
 export const dbconnect = async () => {
     try {
         if (process.env.MONGODB_URL) {
-            await mongoose.connect(process.env.MONGODB_URL)
-            console.log("database connected successfully");
-        }else{
+            await mongoose.connect(process.env.MONGODB_URL,{
+                dbName: "blogUserAuth" // Specify your database name here
+            }) 
+              
+            console.log("database connected successfullyyy");
+  
+        } else {
             throw new Error("MONGODB_URL not defined")
-        }
-        
+        }   
+
     } catch (err) {
         console.log("database connection failed" + err);
     }
