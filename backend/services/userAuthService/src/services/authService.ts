3c286fbeec635b388d3f3f4738c98fc40a3acd61
jitheshm <jitheshmjithooz@gmail.com>
2024-05-19 22:01:38 +0530

    function for token verification is added

---
+++ backend/services/userAuthService/src/services/authService.ts
@@ -0,0 +1,12 @@
+import { verify } from "jsonwebtoken";
+
+export default (token: string) => {
+    const JWT_SECRET = 'userauthsecretkey';
+    try {
+        const decoded = verify(token, JWT_SECRET);
+        return decoded
+    } catch (err) {
+        console.log(err);
+
+    }
+}
