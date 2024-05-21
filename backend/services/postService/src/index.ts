7f68910e60c0d6def57722f578be54798c0aab90
jitheshm <jitheshmjithooz@gmail.com>
2024-05-21 17:34:24 +0530

    post service created

---
+++ backend/services/postService/src/index.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response, Application } from 'express';
+import dotenv from 'dotenv';
+// import router from './routes/router';
+// import { dbconnect } from './config/connection';
+// import logger from 'morgan';
+// import cors from 'cors'
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 5000;
+// app.use(logger('dev'));
+app.use(express.json()); 
+// app.use(cors())
+// dbconnect()
+
+ app.use('/', ()=>{
+    console.log('Hello from the server')
+ })
+
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
