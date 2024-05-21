9acecf5e0a28ce7678680b25a8ba40aab816c2ca
jitheshm <jitheshmjithooz@gmail.com>
2024-05-21 21:17:44 +0530

    database connection setup

--- backend/services/postService/src/index.ts
+++ backend/services/postService/src/index.ts
@@ -1,22 +1,20 @@
 import express, { Express, Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
-// import router from './routes/router';
-// import { dbconnect } from './config/connection';
-// import logger from 'morgan';
-// import cors from 'cors'
+import router from './routes/router';
+import { dbconnect } from './config/dbConnection';
+import logger from 'morgan';
+import cors from 'cors'
 //For env File 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 3001;
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json()); 
-// app.use(cors())
-// dbconnect()
+app.use(cors())
+dbconnect()
 
- app.use('/', ()=>{
-    console.log('Hello from the server')
- })
+ app.use('/',router)
 
 
 app.listen(port, () => {
