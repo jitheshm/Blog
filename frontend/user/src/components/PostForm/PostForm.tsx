aa1b2231b0f801867652f0da0064f82fc5e26943
jitheshm <jitheshmjithooz@gmail.com>
2024-05-21 14:53:52 +0530

    react quill installed, post form updated with quill editor

--- frontend/user/src/components/PostForm/PostForm.tsx
+++ frontend/user/src/components/PostForm/PostForm.tsx
@@ -1,6 +1,35 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'
 
 function PostForm() {
+
+    const [editorContent, setEditorContent] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+
+    const modules = {
+        toolbar: [
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ 'align': [] }], ,
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            ['link']
+        ]
+    };
+
+    const handleEditorChange = (content) => {
+        setEditorContent(content);
+    };
+
+    const handleImageInsert = () => {
+        // Insert the image URL into the editor content
+        const newContent = `${editorContent}<img src="${imageUrl}" alt="Inserted Image" />`;
+        setEditorContent(newContent);
+        //onContentChange(newContent); // Update the parent component with the new content
+    };
+
+
     return (
         <div className='container-fluid'>
             <div className='col-8 m-auto pt-4'>
@@ -12,15 +41,11 @@ function PostForm() {
 
 
                                     <div className="card-body ">
-                                        <div className="mb-md-5 mt-md-4 pb-2">
+                                        <div className=" mt-md-4 pb-1">
                                             <div className='text-center'>
                                                 <h4><b>Create a New Post</b></h4>
                                             </div>
-                                            <div data-mdb-input-init className="form-outline form-white mt-5 mb-2">
-                                                <label className="form-label" htmlFor="text">Title</label>
-                                                <input type="text" id="text" className="form-control form-control-lg" />
 
-                                            </div>
                                             <div data-mdb-input-init className="form-outline form-white mb-2">
                                                 <label className="form-label" htmlFor="text">Tag</label>
                                                 <input type="text" id="text" className="form-control form-control-lg" />
@@ -28,11 +53,24 @@ function PostForm() {
                                             </div>
                                             <div data-mdb-input-init className="form-outline form-white mb-2">
                                                 <label className="form-label" htmlFor="">Description</label>
-                                                <textarea name="" id="" className='col-12' style={{ height: '200px' }}></textarea>
+                                                <div style={{ height: "300px" }}>
+                                                    <ReactQuill
+                                                        theme="snow"
+                                                        modules={modules}
+                                                        value={editorContent}
+                                                        onChange={handleEditorChange}
+                                                        placeholder="Compose an epic..."
+                                                        style={{ height: "250px" }}
+                                                    />
 
-                                            </div>
+
+                                                </div>   
+                                                <input type="text" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} placeholder="Enter image URL here..." />
+                                                <button className='btn btn-success post-btn mx-2 btn-sm' onClick={handleImageInsert}>  Insert Image </button>
+
+                                            </div>   
                                             <div>
-                                                <button className='btn btn-dark'> Post </button>
+                                                <button className='btn btn-dark btn-sm post-btn'> Submit </button>    
                                             </div>
 
                                         </div>
@@ -45,7 +83,7 @@ function PostForm() {
                 </section >
 
 
-                
+
             </div>
         </div>
     )
