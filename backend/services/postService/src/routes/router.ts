4fc26ceb9b681138ad1924d84eae2628d51e3ecf
jitheshm <jitheshmjithooz@gmail.com>
2024-05-22 17:12:24 +0530

    user post fetch route updated

--- backend/services/postService/src/routes/router.ts
+++ backend/services/postService/src/routes/router.ts
@@ -6,8 +6,8 @@ import getPostController from "../controllers/getPostController";
 
 const router = Router();
 
-router.post('/api/posts',authentication,createController)
-router.get('/api/posts/postid/:id',authentication,getPostController)
+router.post('/api/posts', authentication, createController)
+router.get('/api/posts/edit/postid/:id', authentication, getPostController)
 
 
 
