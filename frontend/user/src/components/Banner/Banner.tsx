3ac7d808e76904862cc45b46cc3bca57504b032f
jitheshm <jitheshmjithooz@gmail.com>
2024-05-23 15:21:52 +0530

    fetch all posts to home page

---
+++ frontend/user/src/components/Banner/Banner.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { PostResponse } from '../Post/Post'
+function Banner({ post }: { post: PostResponse['data'][0] }) {
+
+    return (
+        <div className="banner-inner pt-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <div className="thumb after-left-top">
+                            <img src={post?.thumbnail} alt="img" />
+                        </div>
+                    </div>
+                    <div className="col-lg-6 align-self-center">
+                        <div className="banner-details mt-4 mt-lg-0">
+                            <div className="post-meta-single">
+                                <ul>
+                                    <li><a className="tag-base tag-blue" href="#">{post?.tag}</a></li>
+                                    <li className="date"><i className="fa fa-clock-o" />{post?.dateOfPost}</li>
+                                </ul>
+                            </div>
+                            <h2>{post?.title}</h2>
+                            {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p> */}
+                            <a className="btn btn-blue" href="#">Read</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
--- frontend/user/src/components/Post/Post.tsx
+++ frontend/user/src/components/Post/Post.tsx
@@ -1,36 +1,63 @@
-import React from 'react'
+"use client"
+import instance from '@/axios'
+import { AxiosResponse } from 'axios'
+import React, { useEffect, useState } from 'react'
+import Banner from '../Banner/Banner'
+export type PostResponse = {
+    success: boolean
+    data: {
+        _id: string
+        title: string
+        content: string
+        tag: string
+        thumbnail: string
+        dateOfPost: string
+    }[]
 
+}
 function Post() {
+
+
+    const [posts, setPosts] = useState<PostResponse['data']>([])
+    useEffect(() => {
+        instance.get('/api/posts').then((res: AxiosResponse<PostResponse>) => {
+            if (res.data.success) {
+                setPosts(res.data.data)
+            }
+        })
+    }, [])
+
     return (
         <div className="banner-area banner-inner-1 " id="banner">
             {/* banner area start */}
-            <div className="banner-inner pt-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <div className="thumb after-left-top">
-                                <img src="assets/img/post/1.png" alt="img" />
-                            </div>
-                        </div>
-                        <div className="col-lg-6 align-self-center">
-                            <div className="banner-details mt-4 mt-lg-0">
-                                <div className="post-meta-single">
-                                    <ul>
-                                        <li><a className="tag-base tag-blue" href="#">Tech</a></li>
-                                        <li className="date"><i className="fa fa-clock-o" />08.22.2020</li>
-                                    </ul>
-                                </div>
-                                <h2>ReZoom outage left some people locked out.</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <a className="btn btn-blue" href="#">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Banner post={posts[0]} />
             {/* banner area end */}
             <div className="container">
                 <div className="row">
+                    <>
+                        {
+                            posts.map((postObj, index) => {
+                                return (
+                                    <div key={postObj._id} className="col-lg-3 col-sm-6">
+                                        <div className="single-post-wrap style-white">
+                                            <div className="thumb">
+                                                <img src={postObj.thumbnail} alt="img" />
+                                                <a className="tag-base tag-blue" href="#">{postObj.tag}</a>
+                                            </div>
+                                            <div className="details">
+                                                <h6 className="title"><a href="#">{postObj.title}</a></h6>
+                                                <div className="post-meta-single mt-3">
+                                                    <ul>
+                                                        <li><i className="fa fa-clock-o" />{postObj.dateOfPost}</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </>
                     <div className="col-lg-3 col-sm-6">
                         <div className="single-post-wrap style-white">
                             <div className="thumb">
@@ -95,7 +122,7 @@ function Post() {
                             </div>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
