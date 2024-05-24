c6c87ab916b7fbc0c9eaa6d2042d1931aeb338cf
jitheshm <jitheshmjithooz@gmail.com>
2024-05-24 11:03:06 +0530

    notification service created

---
+++ backend/services/notificationService/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Express, Request, Response, Application } from 'express';
+import dotenv from 'dotenv';
+// import router from './router/router'; 
+// import { dbconnect } from './config/dbConnection';
+import logger from 'morgan';
+import cors from 'cors'
+// import rabbitConnect from './config/connectRabbitMQ';
+
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 3003;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors())
+// dbconnect()
+// rabbitConnect()
+
+// app.use('/', router)
+
+
+app.listen(port, () => {
+    console.log(`Server is Fire at http://localhost:${port}`);
+});
